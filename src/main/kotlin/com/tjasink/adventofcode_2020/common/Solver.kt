ef8064c7b4fa122a82a9b3be8f0ad25dae3765da
Stephen Tjasink <stephen@tjasink.com>
2020-12-12 11:08:55 +0000

    Refactor

---
+++ src/main/kotlin/com/tjasink/adventofcode_2020/common/Solver.kt
@@ -0,0 +1,6 @@
+package com.tjasink.adventofcode_2020.common
+
+interface Solver {
+    fun part1(input: List<String>): Long
+    fun part2(input: List<String>): Long
+}
--- src/main/kotlin/com/tjasink/adventofcode_2020/puzzle_12/Day12.kt
+++ src/main/kotlin/com/tjasink/adventofcode_2020/puzzle_12/Day12.kt
@@ -1,27 +1,19 @@
 package com.tjasink.adventofcode_2020.puzzle_12
 
+import com.tjasink.adventofcode_2020.common.Solver
 import com.tjasink.adventofcode_2020.common.loadInput
-import com.tjasink.adventofcode_2020.common.timeCode
+import com.tjasink.adventofcode_2020.common.runAndTime
 import kotlin.math.absoluteValue
 
 fun main() {
     val input = loadInput("puzzle_12/data.txt")
     val solver = Day12()
-
-    val part1Answer = solver.part1(input)
-    println("Part 1 answer: $part1Answer")
-    val part1Time = timeCode { solver.part1(input) }
-    println("Part 1 time: $part1Time / ${part1Time.toMillis()}ms / ${part1Time.toNanos() / 1000}μs")
-
-    val part2Answer = solver.part2(input)
-    println("Part 2 answer: $part2Answer")
-    val part2Time = timeCode { solver.part2(input) }
-    println("Part 2 time: $part2Time / ${part2Time.toMillis()}ms / ${part2Time.toNanos() / 1000}μs")
+    runAndTime(solver, input)
 }
 
-class Day12 {
+class Day12: Solver {
 
-    fun part1(input: List<String>): Int {
+    override fun part1(input: List<String>): Long {
         var x = 0
         var y = 0
         var heading = 90
@@ -46,10 +38,10 @@ class Day12 {
                 }
             }
 
-        return x.absoluteValue + y.absoluteValue
+        return (x.absoluteValue + y.absoluteValue).toLong()
     }
 
-    fun part2(input: List<String>): Int {
+    override fun part2(input: List<String>): Long {
         var x = 0
         var y = 0
         var wpX = 10
@@ -72,7 +64,7 @@ class Day12 {
                 }
             }
 
-        return x.absoluteValue + y.absoluteValue
+        return (x.absoluteValue + y.absoluteValue).toLong()
     }
 
     private fun rotateRight(x: Int, y: Int, degrees: Int): Pair<Int, Int> {
