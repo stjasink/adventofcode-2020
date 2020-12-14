f3edf13fd6ccf142210a31d26f651e33e6c62bf3
Stephen Tjasink <stephen@tjasink.com>
2020-12-14 06:13:49 +0000

    Day 13 not finished

--- src/main/kotlin/com/tjasink/adventofcode_2020/puzzle_13/Day13.kt
+++ src/main/kotlin/com/tjasink/adventofcode_2020/puzzle_13/Day13.kt
@@ -10,17 +10,89 @@ fun main() {
     runAndTime(solver, input)
 }
 
-class Day13: Solver {
+class Day13 : Solver {
 
     override fun part1(input: List<String>): Long {
+        val myEarliestArrival = input[0].toInt()
+        val busIds = input[1].split(',').filterNot { it == "x" }.map { it.toInt() }
+        val firstBusArrivalsAfterMyArrival = busIds.map { busId -> firstArrivalAfter(myEarliestArrival, busId) }
+        val earliestBusAndWaitTime = busIds.zip(firstBusArrivalsAfterMyArrival)
+            .map { Pair(it.first, it.second - myEarliestArrival) }
+            .sortedBy { it.second }
+            .first()
 
-        return 0
+        return earliestBusAndWaitTime.first.toLong() * earliestBusAndWaitTime.second.toLong()
     }
-
 
     override fun part2(input: List<String>): Long {
+        return part2v2(input)
 
-        return 0
+        val busIds = input[1].split(',').map { if (it == "x") null else it.toLong() }
+        val runningTotals = mutableListOf<Long?>().apply { this.addAll(busIds) }
+
+        while (true) {
+            runningTotals[0] = runningTotals[0]!! + busIds[0]!!
+            var highestBusMatched = 0
+            for (i in 1 until runningTotals.size) {
+                if (runningTotals[i] == null) {
+                    continue
+                }
+                while (runningTotals[i]!! < runningTotals[0]!! + i) {
+                    runningTotals[i] = runningTotals[i]!! + busIds[i]!!
+                }
+                if (runningTotals[i] == runningTotals[0]!! + i) {
+                    highestBusMatched = i
+                    continue
+                } else {
+                    break
+                }
+            }
+            if (highestBusMatched == busIds.size - 1) {
+                return runningTotals[0]!!
+            }
+            if (highestBusMatched > 32) {
+                println(runningTotals[0])
+                println(highestBusMatched)
+            }
+        }
     }
 
+    fun part2v2(input: List<String>): Long {
+        val busIds = input[1].split(',').map { if (it == "x") null else it.toLong() }
+        val mustBeDivisibleBy = busIds
+            .mapIndexed { index, busId -> Pair(index, busId) }
+            .filter { it.second != null }
+            .map { it.first to it.second!! }
+
+        val incrementBy = mustBeDivisibleBy.map { it.second }.first()
+//        val incrementBy = mustBeDivisibleBy.sortedByDescending { it.second }.first().let { it.second + it.first }
+        var startTime = incrementBy
+        while (true) {
+//            println(startTime)
+            val currentlyDivisible = mustBeDivisibleBy.map { (gap, busId) ->
+                (startTime + gap) % busId == 0L
+            }
+            if (!currentlyDivisible.contains(false)) {
+                return startTime
+            }
+            startTime += incrementBy
+        }
+    }
+
+    fun part2v3(input: List<String>): Long {
+        val busIds = input[1].split(',').map { if (it == "x") null else it.toLong() }
+        val mustBeDivisibleBy = busIds
+            .mapIndexed { index, busId -> Pair(index, busId) }
+            .filter { it.second != null }
+            .map { it.first to it.second!! }
+        
+    }
+
+    private fun firstArrivalAfter(time: Int, busId: Int): Int {
+        var arrivalTime = 0
+        while (arrivalTime < time) {
+            arrivalTime += busId
+        }
+        return arrivalTime
+    }
 }
--- src/test/kotlin/com/tjasink/adventofcode_2020/puzzle_13/Day13Test.kt
+++ src/test/kotlin/com/tjasink/adventofcode_2020/puzzle_13/Day13Test.kt
@@ -5,17 +5,38 @@ import org.junit.jupiter.api.Test
 
 class Day13Test {
 
-    val input = """""".trimIndent().split('\n')
+    val input = """
+        939
+        7,13,x,x,59,x,31,19
+    """.trimIndent().split('\n')
 
     @Test
     fun part1Test() {
         val answer = Day13().part1(input)
-        Assertions.assertEquals(25, answer)
+        Assertions.assertEquals(295, answer)
     }
 
     @Test
     fun part2Test() {
         val answer = Day13().part2(input)
-        Assertions.assertEquals(286, answer)
+        Assertions.assertEquals(1068781, answer)
+    }
+
+    @Test
+    fun part2TestDifferentExamples() {
+        val inputs = listOf(
+            "17,x,13,19" to 3417L,
+            "67,7,59,61" to 754018L,
+            "67,x,7,59,61" to 779210L,
+            "67,7,x,59,61" to 1261476L,
+            "1789,37,47,1889" to 1202161486L
+        )
+
+        inputs.forEach {
+            val input = listOf("ignored") + it.first
+            val answer = Day13().part2(input)
+            Assertions.assertEquals(it.second, answer)
+        }
+
     }
 }
