4a8b5809229d421b4dd50440397d84df4a91b737
Stephen Tjasink <stephen@tjasink.com>
2020-12-15 06:32:14 +0000

    Day 15 use Int instead of Long

--- src/main/kotlin/com/tjasink/adventofcode_2020/puzzle_15/Day15.kt
+++ src/main/kotlin/com/tjasink/adventofcode_2020/puzzle_15/Day15.kt
@@ -13,35 +13,30 @@ fun main() {
 class Day15 : Solver {
 
     override fun part1(input: List<String>): Long {
-        val startNumbers = input.first().split(',').map { it.toLong() }
-        return playGame(startNumbers, 2020)
+        val startNumbers = input.first().split(',').map { it.toInt() }
+        return playGame(startNumbers, 2020).toLong()
     }
 
     override fun part2(input: List<String>): Long {
-        val startNumbers = input.first().split(',').map { it.toLong() }
-        return playGame(startNumbers, 30000000)
+        val startNumbers = input.first().split(',').map { it.toInt() }
+        return playGame(startNumbers, 30000000).toLong()
     }
 
-    private fun playGame(withStartNumbers: List<Long>, untilTurn: Long): Long {
-
-        val lastSeenAt = mutableMapOf<Long, Long>()
-
+    private fun playGame(withStartNumbers: List<Int>, untilTurn: Int): Int {
+        val lastSeenAt = mutableMapOf<Int, Int>()
         for (i in 0 until withStartNumbers.size - 1) {
-            lastSeenAt[withStartNumbers[i]] = i.toLong()
+            lastSeenAt[withStartNumbers[i]] = i
         }
         var turnNumber = withStartNumbers.size
         var newNumber = withStartNumbers.last()
 
         while (true) {
-            if (turnNumber % 1000000 == 0) {
-                println(turnNumber)
-            }
-            if (turnNumber.toLong() == untilTurn) {
+            if (turnNumber == untilTurn) {
                 return newNumber
             }
 
             val foundAt = lastSeenAt[newNumber]
-            lastSeenAt[newNumber] = turnNumber.toLong() - 1
+            lastSeenAt[newNumber] = turnNumber - 1
 
             if (foundAt == null) {
                 newNumber = 0
